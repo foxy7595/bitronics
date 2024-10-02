828843a12ad38622ed9f5bccadfa639344a93e48
Foxy <taikhoan759501@gmail.com>
2024-10-02 08:35:17 +0700

    fix: build 2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import Viewer3D from "@/components/3dViewer";
 const AboutPage: React.FC = () => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <Viewer3D modelPath="/models/service-1.glb" />
+      {/* <Viewer3D modelPath="/models/service-1.glb" /> */}
     </div>
   );
 };
